8bac3da08bfc7ad84e45b6ef1ee8f75c35015bec
Andy Chung <andy6756@gmail.com>
2022-11-15 01:17:28 +0800

    Add Router Function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
-import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
+import { RecordComponent } from './record/record.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'record',
+    component: RecordComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: "**",
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+    enableTracing: true,
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
+import { RecordComponent } from './record/record.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecordComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
